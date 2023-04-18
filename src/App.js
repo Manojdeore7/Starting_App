5f3f43ff0c25d7c117c81a3659feabebea689b1d
Manojdeore7 <manojddeore11>
2023-04-18 16:30:05 +0530

    cancel button

--- src/App.js
+++ src/App.js
@@ -6,24 +6,32 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [err, setError] = useState(null);
   async function customMovies() {
-    setIsLoading(true);
-    const res = await fetch("https://reqres.in/api/users?page=2");
-    const dat = await res.json();
-    let inf = dat.data.map((e) => {
-      return {
-        id: e.id,
-        title: e.email,
+    try {
+      setIsLoading(true);
+      setError(null);
+      const res = await fetch("https://reqres.in/api/");
+      if (!res.ok) {
+        throw new Error("something wennt wrong ....Retrying!");
+      }
+      const dat = await res.json();
+      let inf = dat.data.map((e) => {
+        return {
+          id: e.id,
+          title: e.email,
 
-        releaseDate: e.first_name,
+          releaseDate: e.first_name,
 
-        openingText: e.last_name,
-      };
-    });
+          openingText: e.last_name,
+        };
+      });
 
-    setIsLoading(false);
-    setMovies(inf);
+      setIsLoading(false);
+      setMovies(inf);
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
   return (
@@ -35,7 +43,14 @@ function App() {
         {isLoading && <p>Loading...</p>}
         {movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && <p>There is no data</p>}
+        {err && <p>{err}</p>}
       </section>
+
+      {isLoading && (
+        <section>
+          <button onClick={customMovies}>Cancel</button>
+        </section>
+      )}
     </React.Fragment>
   );
 }
